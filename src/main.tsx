82d4a3297604e6aecc717581f0dcbcebf41e11fa
Pavel Taiko <paveltaiko@icloud.com>
2026-02-20 13:24:46 +0100

    Configure Google Analytics to run only in production

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,30 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+const GA_MEASUREMENT_ID = 'G-J7KLN7Z4TD'
+
+declare global {
+  interface Window {
+    dataLayer: unknown[]
+    gtag: (...args: unknown[]) => void
+  }
+}
+
+if (import.meta.env.PROD) {
+  window.dataLayer = window.dataLayer || []
+  window.gtag = (...args: unknown[]) => {
+    window.dataLayer.push(args)
+  }
+
+  window.gtag('js', new Date())
+  window.gtag('config', GA_MEASUREMENT_ID)
+
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+  document.head.appendChild(script)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
